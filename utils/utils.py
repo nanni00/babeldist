31c9f0eb457774038f411cad9b0388b6b5c9d645
nanni00 <giovanni.malaguti00@gmail.com>
2023-09-04 10:38:04 +0200

    Untrack files

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,7 @@ import os
 
 _log_dir_rel_path = '../log/'
 
+
 def get_current_logfile_number(fname, extension='.log'):
     ncpy = 1
     if not os.path.exists(_log_dir_rel_path + fname + extension):
@@ -10,12 +11,14 @@ def get_current_logfile_number(fname, extension='.log'):
         while os.path.exists(f'{_log_dir_rel_path}{fname}({ncpy}){extension}'):
             ncpy += 1
         return f'{_log_dir_rel_path}{fname}({ncpy}){extension}'
-    
+
+
 def get_full_class_name(obj):
     module = obj.__class__.__module__
     if module is None or module == str.__class__.__module__:
         return obj.__class__.__name__
     return module + '.' + obj.__class__.__name__
+
 
 def get_progress_bar(perc, len_bar=100):
     return str(perc) + ' ' +u"\u2588"*int(perc//10)
