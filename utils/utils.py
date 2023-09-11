48e266250595c155c723329592f48ab477c76f99
nanni00 <giovanni.malaguti00@gmail.com>
2023-09-11 10:04:29 +0200

    More results on distance measures

--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,17 @@
 import os
 
 _log_dir_rel_path = '../log/'
-
-
 def get_current_logfile_number(fname, extension='.log'):
+    ncpy = 1
+    if not os.path.exists(_log_dir_rel_path + fname + extension):
+        return _log_dir_rel_path + fname + extension
+    else:
+        while os.path.exists(f'{_log_dir_rel_path}{fname}({ncpy}){extension}'):
+            ncpy += 1
+        return f'{_log_dir_rel_path}{fname}({ncpy-1}){extension}'
+
+
+def get_next_logfile_number(fname, extension='.log'):
     ncpy = 1
     if not os.path.exists(_log_dir_rel_path + fname + extension):
         return _log_dir_rel_path + fname + extension
